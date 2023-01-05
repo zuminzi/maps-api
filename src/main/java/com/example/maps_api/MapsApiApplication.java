89d98896d6ab9529ea9b95a7ab0006042ea71005
Ju Minji <alswl0w0@gmail.com>
2023-01-05 21:25:45 +0900

    setting

---
+++ src/main/java/com/example/maps_api/MapsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.maps_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapsApiApplication.class, args);
+    }
+
+}
